d23bb0ec7173793b598800d96559c66f03e6b582
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-10-20 17:47:18 -0600

    rafactor parent component to have handle click function defined in parent

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ import TextRepeaterButton from './components/TextRepeatButton';
 function App() {
 
   const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+
+  const switchLight = () => setLight((light === "on") ? "off" : "on");
 
   return (
-    <div className={`App dark`} >
+    <div className={`App ${dark}`} >
       <h1>Fancy Buttons!</h1>
       <section>
      <AngryButton />
      <CounterButton />
-     <LightSwitchButton light={light} setLight={setLight}/>
+     <LightSwitchButton light={light} setLight={setLight} switchLight={switchLight}/>
      <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -2,15 +2,9 @@ import React, { useState } from 'react'; //optional
 
   function LightSwitchButton(props){
 
-    const {light, setLight} = props;
+    const {light, switchLight} = props;
 
-    const handleClick = () => {
-      if (light === 'off') {
-        setLight('on')
-      } else {
-        setLight('off')
-      }
-    }
+    const handleClick = () => switchLight();
 
     return (
       <button onClick={handleClick} className="LightSwitchButton">
